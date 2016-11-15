a6334219924ece5e76e993b38f06f7fd1e7606d5
Elizabeth Kelly <Elizabeth.Kelly25@myhunter.cuny.edu>
2016-11-15 16:19:20 -0500

    Implemented image grayscale conversion and conversion to binary

--- Coin Detector/main.cpp
+++ Coin Detector/main.cpp
@@ -14,22 +14,34 @@ using namespace std;
 using namespace cv;
 
 int main(int argc, char** argv ) {
+    
     if ( argc != 2 )
     {
         printf("usage: DisplayImage.out <Image_Path>\n");
         return -1;
     }
     
-    Mat image;
-    image = imread( argv[1], 1 );
+    String imageFile = argv[1]; //filename for the coin image
+    Mat orig_image = imread(imageFile); //read the file
+    resize(orig_image, orig_image, cvSize(378, 504)); //resize the image
     
-    if ( !image.data )
+    if ( !orig_image.data )
     {
         printf("No image data \n");
         return -1;
     }
-    namedWindow("Display Image", WINDOW_AUTOSIZE );
-    imshow("Display Image", image);
+
+    //convert the image to grayscale
+    Mat gray_image;
+    cvtColor(orig_image, gray_image, CV_BGR2GRAY);
+
+    //threshold the image to convert it to binary
+    Mat binary_image;
+    binary_image = gray_image > 100;
+    String window_name = "Display Image";
+
+    imshow(window_name, gray_image);
+    namedWindow(window_name, WINDOW_NORMAL );
     
     waitKey(0);
     
