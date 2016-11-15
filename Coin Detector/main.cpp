adefd39496a8073d4be00526428624c73d45de82
William Wu <will.go.code@gmail.com>
2016-11-15 00:45:15 -0500

    Initialized repository with base code.

---
+++ Coin Detector/main.cpp
@@ -0,0 +1,37 @@
+//
+//  main.cpp
+//  Coin Detector
+//
+//  Created by William Wu on 11/14/16.
+//  Copyright © 2016 war. All rights reserved.
+//
+
+#include <stdio.h>
+#include <iostream>
+#include <opencv2/opencv.hpp>
+
+using namespace std;
+using namespace cv;
+
+int main(int argc, char** argv ) {
+    if ( argc != 2 )
+    {
+        printf("usage: DisplayImage.out <Image_Path>\n");
+        return -1;
+    }
+    
+    Mat image;
+    image = imread( argv[1], 1 );
+    
+    if ( !image.data )
+    {
+        printf("No image data \n");
+        return -1;
+    }
+    namedWindow("Display Image", WINDOW_AUTOSIZE );
+    imshow("Display Image", image);
+    
+    waitKey(0);
+    
+    return 0;
+}
